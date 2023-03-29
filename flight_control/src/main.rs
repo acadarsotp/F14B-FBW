3dc32fd38b72d2d5d51644a790514e1428769862
acada <acadarsotp@gmail.com>
2023-03-29 18:32:46 +0200

    get hotas input data through tcp

--- flight_control/src/main.rs
+++ flight_control/src/main.rs
@@ -1,53 +1,32 @@
-use std::io::{BufRead, BufReader};
-use std::net::TcpListener;
 use serde::Deserialize;
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+mod optz;
+use optz::Input;
 
-#[derive(Debug, Deserialize)]
-struct InputData {
-    time: u64,
-    axis_0: f32,
-    axis_1: f32,
-    axis_2: f32,
-    axis_3: f32,
-    button_0: u8,
-    button_1: u8,
-    button_2: u8,
-    button_3: u8,
-    button_4: u8,
-    button_5: u8,
-    button_6: u8,
-    button_7: u8,
-    button_8: u8,
-    button_9: u8,
-    button_10: u8,
-    button_11: u8,
-    hat_0: String,
-}
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:3333")?;
+    println!("Server listening on port 3333");
 
+    // accept connections and process them, spawning a new thread for each one
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                let mut reader = BufReader::new(stream);
-                let mut line = String::new();
-                while reader.read_line(&mut line).unwrap() > 0 {
-                    match serde_json::from_str::<InputData>(&line) {
-                        Ok(input_data) => {
-                            println!("{:?}", input_data);
-                        }
-                        Err(e) => {
-                            eprintln!("Error parsing input data: {:?}", e);
-                        }
-                    }
-                    line.clear();
-                }
+                println!("New connection: {}", stream.peer_addr()?);
+                std::thread::spawn(move || {
+                    // connection succeeded
+                    optz::handle_client(stream).unwrap_or_else(|error| {
+                        eprintln!("Error: {}", error);
+                    });
+                });
             }
             Err(e) => {
-                eprintln!("Error connecting to client: {:?}", e);
+                // connection failed
+                eprintln!("Error: {}", e);
             }
         }
     }
+    Ok(())
 }
 
---
+++ flight_control/src/optz.rs
@@ -0,0 +1,65 @@
+use serde::Deserialize;
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+
+#[derive(Deserialize, Debug)]
+pub(crate) struct Input {
+    time: u32,
+    axis_0: f32,
+    axis_1: f32,
+    axis_2: f32,
+    axis_3: f32,
+    axis_4: f32,
+    button_0: u8,
+    button_1: u8,
+    button_2: u8,
+    button_3: u8,
+    button_4: u8,
+    button_5: u8,
+    button_6: u8,
+    button_7: u8,
+    button_8: u8,
+    button_9: u8,
+    button_10: u8,
+    button_11: u8,
+    hat_0: (i8, i8),
+}
+
+pub(crate) fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
+    // Create a new vector to store incoming data from the client
+    let mut data = Vec::new();
+
+    // Loop indefinitely to read data from the stream
+    loop {
+        // Create a buffer to read incoming data
+        let mut buf = [0; 80000];
+
+        // Read data from the stream and check if the read operation succeeded and if any data was read
+        match stream.read(&mut buf) {
+            Ok(size) if size > 0 => {
+                // Extend the data vector with the read data
+                data.extend_from_slice(&buf[..size]);
+
+                // Try to deserialize the received data into an Input struct
+                if let Ok(input) = serde_json::from_slice::<Input>(&data) {
+                    // If deserialization was successful, print the Input struct
+                    println!("{:?}", input);
+
+                    // Clear the data vector for the next read operation
+                    data.clear();
+                }
+            }
+            // If the read operation succeeded but no data was read, continue reading
+            Ok(_) => continue,
+            // If the read operation failed, check if the stream was disconnected
+            Err(e) if e.kind() == std::io::ErrorKind::ConnectionReset => {
+                println!("Client disconnected");
+                break;
+            }
+            // If the read operation failed and the stream was not disconnected, return an error
+            Err(_) => return Err(std::io::ErrorKind::InvalidData.into()),
+        }
+    }
+
+    Ok(())
+}
